c3faa17581474c0cf45831e9c09912ed94aaadaf
oualter <olivier.pretre@gmx.fr>
2023-10-25 19:12:32 +0200

    test git

---
+++ config/api.ts
@@ -0,0 +1,7 @@
+export default {
+  rest: {
+    defaultLimit: 200,
+    maxLimit: 200,
+    withCount: true,
+  },
+};
---
+++ config/plugins.ts
@@ -0,0 +1,8 @@
+export default {
+
+  "image-pinpoint-generator": {
+    enabled: true,
+    resolve: "./src/plugins/image-pinpoint-generator",
+  },
+
+};
