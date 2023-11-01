32ac69d332ab35f115c9803a8f28c674bf0045b2
oualter <olivier.pretre@gmx.fr>
2023-11-01 15:28:11 +0100

    import export plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,9 @@
 export default {
-
   "image-pinpoint-generator": {
     enabled: true,
     resolve: "./src/plugins/image-pinpoint-generator",
   },
-
+  "import-export-entries": {
+    enabled: true,
+  },
 };
