ba0b155966a523eac15f20ff47a32dfa528be380
oualter <olivier.pretre@gmx.fr>
2023-11-03 17:01:34 +0100

    erase node engine version

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,5 +8,12 @@ export default {
   },
   "vercel-deploy": {
     enabled: true,
+    config: {
+    //   deployHook:        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "lRIoQ1CYY0gA64iC6shBxbFU",
+    //   appFilter: "your-app-name-on-vercel",
+    //   teamFilter: "your-team-id-on-vercel",
+    //   roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
   },
 };
